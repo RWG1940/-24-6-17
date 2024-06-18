f8c12f177a8bb44b41a79463fac94c7fd7b0f99a
RWG <1940449715@qq.com>
2024-06-18 13:04:50 +0800

    Reg

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
-
 <template>
   <div class="App">
-    <div class="login">
-        <button @click="loginButton">登录</button>
-    </div>
     <RouterView />
   </div>
 </template>
 <script setup lang="ts">
-import router from '@/router/router'
-const loginButton = () => {
-    router.replace('login')
-}
+
 </script>
 <style scoped>
 
--- src/views/Reg.vue
+++ src/views/Reg.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
+    <div class="Reg">
+        <div class="reg-card">
+            <h1>注册</h1>
+            <el-input type="text" placeholder="账号"></el-input>
+            <el-input type="password" placeholder="密码"></el-input>
+            <el-button type="primary">登录</el-button>
+        </div>
+       
+
     </div>
   
 </template>
@@ -7,4 +15,11 @@
 
 </script>
 <style scoped>
+.Reg {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
 </style>
